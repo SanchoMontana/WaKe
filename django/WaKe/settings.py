46f73b7d8d50007fe57f2fdb1cf6e4a26bbdba0b
Sancho Montana <sancho.montanakbk@gmail.com>
2025-03-29 23:52:55 -0400

    Docker compose works fine now. Functional, yet bare-bones docker container for django, react, and postgresql.

--- django/WaKe/settings.py
+++ django/WaKe/settings.py
@@ -68,22 +68,20 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'WaKe.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'wake_db',
+#         'USER': 'user',
+#         'PASSWORD': 'user',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
